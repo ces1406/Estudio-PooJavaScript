6047b3278b82077c19e0267b212920eb3ba43d8f
cesar montalvan <ces1406@gmail.com>
2020-10-15 10:52:39 -0300

    observando el prototype

--- 4-metodos-Object.js
+++ 4-metodos-Object.js
@@ -9,11 +9,17 @@ console.log('objeto1: ',objeto1)
 
 // Object.create():
 console.log("\x1b[34m%s\x1b[0m",'Forma 6: uso de Object.create(prototipo,atributos):');
-var objeto2 = Object.create(objeto1,{propiedad3:{value:'valor para propiedad3',writable:true,enumerable:true,configurable:true}});
-// nota: NO hereda las propiedades del objeto indicado como prototipo (=>para que sirve?)
-console.log('objeto2: ',objeto2)
+var objeto2 = {
+    propiedad21:'una propiedad string',
+    metodo21: function(){ console.log('->metodo41->con propiedad41: '+this.propiedad21)} //NOTAR->uso de "this"
+}
+objeto2.metodo21();
+var objeto3 = Object.create(objeto2,{propiedad22:{configurable:true,writable:true,value:'otro string',enumerable:true}})
+console.log('objeto5->propiedad22: ',objeto3.propiedad22);
+console.log('objeto5->propiedad21(en su prototype): ',objeto3.propiedad21)
+objeto3.metodo21()
 
 // Copiando/clonando otro objeto con Object.assign(destino,fuente):
 console.log("\x1b[34m%s\x1b[0m",'Forma 7: uso de Object.assign(objetoDestino,objetoFuente):');
-var objeto3 = Object.assign({},objeto1)
-console.log('objeto3: ',objeto3)
+var objeto4 = Object.assign({},objeto1)
+console.log('objeto3: ',objeto4)
