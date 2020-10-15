c107afb54a13b0b2530166f0ddd30d0ed4cf7051
cesar montalvan <ces1406@gmail.com>
2020-10-15 12:18:25 -0300

    fixed class, getters and setters in es6 style

--- 7-es6.js
+++ 7-es6.js
@@ -3,29 +3,22 @@ console.log("\x1b[36m%s\x1b[0m", 'OBJETOS:\n' + '-------------------------------
 console.log("\x1b[34m%s\x1b[0m",'Forma 10: uso de class (ES6):');
 
 class Clase1 {
-    propiedad1 = 'variable de clase?';
-    propiedad2 = 99;
-    
-    metodo1(param){
-        console.log('Clase1->metodo1-> parametro->'+param)
-    }
+    constructor(prop1,prop2){
+        this.propiedad1 = prop1 || 'vacio';
+        this.propiedad2 = prop2 || 99;   
+    }     
+    metodo1(param){ console.log('Clase1->metodo1-> parametro: '+param) }
     // getters y setters:
-    get propiedad1 () {
-        return 'PROP1->'+this.propiedad1;
-    }
-    get propiedad2 () {
-        return 'PROP2->'+this.propiedad2;
-    }
-    set propiedad1 (p1) {
-        this.propiedad1 = p1;
-    }
-    set propiedad2 (p2) {
-        this.propiedad2 = p2;
-    }
+    // NOTA: en los settters/getters si no se utiliza el _ provoca que se la función se
+    // llame a sí misma recursivamente e infinitamente
+    get propiedad1 () { return 'PROP1->'+this._propiedad1; }
+    get propiedad2 () { return 'PROP2->'+this._propiedad2; }
+    set propiedad1 (p1) { this._propiedad1 = '1**'+p1+'**1'; }
+    set propiedad2 (p2) { this._propiedad2 = '2**'+p2+'**2'; }
 }
 
 var objeto1 = new Clase1();
-console.log('objeto1.propiedad1 = '+objeto1.propiedad1);
 objeto1.metodo1('cualquier cosa');
+console.log('objeto1.propiedad1 = '+objeto1.propiedad1);
 objeto1.propiedad1 = 'otra cadena';
-console.log('objeto1.propiedad1 = '+objeto1.propiedad1)
+console.log('objeto1.propiedad1 = '+objeto1.propiedad1);
